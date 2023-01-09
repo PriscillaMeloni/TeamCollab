8201995c2a8f38e70d367abfeff86a7057e46d5d
erumkaleemullah <erumkashif@hotmail.com>
2023-01-08 18:20:51 -0600

    First and second class files are added

---
+++ src/Erum_Folder/FirstClass.java
@@ -0,0 +1,8 @@
+package Erum_Folder;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hi Team we did it we finished Java");
+    }
+
+}
---
+++ src/Erum_Folder/SecondClass.java
@@ -0,0 +1,9 @@
+package Erum_Folder;
+
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Everyone is ready for automation?");
+        System.out.println("All the best!");
+    }
+
+}
