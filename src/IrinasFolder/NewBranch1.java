25ae6991e62b389cb825c6c50d25256b5154d1e1
irene <irene_ste@yahoo.com>
2023-01-12 01:25:29 -0900

    new class

---
+++ src/IrinasFolder/NewBranch1.java
@@ -0,0 +1,18 @@
+package IrinasFolder;
+
+public class NewBranch1 {
+
+    public static void main(String[] args) {
+
+        String mood = "Tired";
+        if(mood.equals("Happy")){
+            System.out.println("I Love Java");
+        } else {
+            System.out.println("I need coffee");
+        }
+
+
+    }
+
+
+        }
